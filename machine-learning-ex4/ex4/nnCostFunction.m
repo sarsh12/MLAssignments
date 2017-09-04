736a0505b8d5a044fb4ddb19c80840460a989111
sarsh12 <shivasundaram1@gmail.com>
2017-09-03 21:48:33 -0400

    assignment 4 completed

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -24,11 +24,33 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
+a1 = [ones(size(X,1),1) X];
+z2 = a1*Theta1';
+a2 = [ones(size(sigmoid(z2),1),1) sigmoid(z2)];
+z3 = a2*Theta2';
+h = sigmoid(z3);
+a3=h;
+y_matrix = eye(num_labels)(y,:) ;
          
 % You need to return the following variables correctly 
-J = 0;
-Theta1_grad = zeros(size(Theta1));
-Theta2_grad = zeros(size(Theta2));
+J = (1/m).*sum(sum((-log(h).*y_matrix) -(log(1-h).*(1-y_matrix))));
+J = J + (lambda/(2*m))*( sum(sum(Theta1(:,2:end).^2)) +sum(sum(Theta2(:,2:end).^2)));
+
+%for t = 1:1;
+%  a1=[1 X(t,:)];
+%  z2 = a1*Theta1';
+%  a2 = [1 sigmoid( z2)];
+%  z3 = a2*Theta2';
+%  a3 = sigmoid(z3);
+  d3  = a3 - y_matrix;
+  d2 = d3*Theta2(:,2:end).*sigmoidGradient(z2);
+  delta_1 = d2'*a1;
+  delta_2 = d3'*a2;
+%end
+Theta1(:,1)=0;
+Theta2(:,1)=0;
+Theta1_grad = 1/m.*delta_1 + (lambda/m)*Theta1;
+Theta2_grad = 1/m.*delta_2+(lambda/m)*Theta2;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should complete the code by working through the
--- machine-learning-ex4/ex4/randInitializeWeights.m
+++ machine-learning-ex4/ex4/randInitializeWeights.m
@@ -8,9 +8,9 @@ function W = randInitializeWeights(L_in, L_out)
 %   Note that W should be set to a matrix of size(L_out, 1 + L_in) as
 %   the first column of W handles the "bias" terms
 %
-
+epsilon_init = 0.12;
 % You need to return the following variables correctly 
-W = zeros(L_out, 1 + L_in);
+W = rand(L_out, 1 + L_in).*(2*(epsilon_init) - epsilon_init);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Initialize W randomly so that we break the symmetry while
